3a67db21d0c59e036933f0931c13fb982bb7e071
Stephen F. Booth <me@sbooth.org>
2024-05-20 09:56:17 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "COgg",
+	products: [
+		.library(
+			name: "ogg",
+			targets: [
+				"ogg",
+			]),
+	],
+	targets: [
+		.target(
+			name: "ogg"),
+	]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# COgg
+
+[Ogg](https://gitlab.xiph.org/xiph/ogg) packaged for the [Swift Package Manager](https://swift.org/package-manager/).
